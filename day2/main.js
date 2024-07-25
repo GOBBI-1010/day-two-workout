f9875354a4e4a6d8f52b9aa2fe5208286bc477fd
Gobbi <dgobi2002@gmail.com>
2024-07-25 10:33:22 +0530

    Add files via upload

---
+++ day2/main.js
@@ -0,0 +1,76 @@
+/*function sayhello(name) {
+    console.log('hello', name);
+}
+
+sayhello('krishna');
+// sayhello('raja');*/
+
+/*
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello")*/
+/*let a = 5
+b=6
+console.log(a + b);*/
+//console.log(5 + 3 * 4 - 2);
+//define datatype-datatype:number\
+/*let number = 5;
+console.log(typeof (number));
+
+let value= 5;
+console.log(typeof (value));*/
+
+//data type=string
+//let firstname = "javascript demo";
+
+//console.log(typeof (firstname));
+
+//data type=boolean
+//let waterbottlehaswater = true;
+
+//undefine
+//let x;console.log(x);
+
+//nul
+//let x = null;
+/*let a = 7
+b = 8
+console.log(a + b);*/
+
+//let age = 25;console.log(age);
+/*for (let i = 5; i <= 9; i++)
+{
+    console.log('hello bharathi');
+}*/
+
+//undefined
+//let x;
+//console.log(x);
+
+
+// datatype:object
+//let number = [1, 2, 3,'no','yes','javascript'];
+//console.log(typeof (number));
+
+//another object type
+
+// let person={
+//     name: 'ravi',
+//     age: 30
+// }
+// console.log(typeof (person));
+
+//datatype:function
+// function sayHello() {
+//     console.log('hello');
+// }
+// console.log(typeof (sayHello));
+
+// types:
+// Number
+// string
+// Boolean
+// Object
+// function
+    
